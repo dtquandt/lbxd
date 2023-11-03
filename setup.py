8c7c0816e5e818efcf91bf43df01da99ef556d86
dtquandt <danieltquandt@gmail.com>
2023-11-02 21:45:06 -0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
    name='lbxd',
    version='0.1',
+   packages=find_packages(),
    description='Wrapper around the letterboxd library which is itself a wrapper around the Letterboxd API ',
    author='Daniel Quandt',
    author_email='danieltquandt@gmail.com',
-   packages=['lbxd'],  #same as name
    install_requires=['pandas', 'pybase62', 'letterboxd', 'python-dotenv'], #external packages as dependencies
 )
