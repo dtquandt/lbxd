692fad2cbd3a4b871beaa92d2326c472cb1183fe
Daniel Quandt <danieltquandt@gmail.com>
2025-05-05 22:16:25 -0300

    Updated version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
    name='lbxd',
-   version='0.1',
+   version='0.2',
    py_modules=['lbxd'],
-   description='Wrapper around the letterboxd library which is itself a wrapper around the Letterboxd API ',
+   description='Wrapper around the letterboxd library which is itself a wrapper around the Letterboxd API',
    author='Daniel Quandt',
    author_email='danieltquandt@gmail.com',
    install_requires=['pandas', 'pybase62', 'letterboxd', 'python-dotenv'], #external packages as dependencies
